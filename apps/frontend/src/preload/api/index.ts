48783ffe4d29f829ea0bf6a1f6cac3fd36e5bce0
kevin rajan <veeman961@gmail.com>
2025-12-26 20:33:15 -0600

    auto-claude: subtask-3-4 - Export explorer API in preload API index
    - Import ExplorerAPI and createExplorerAPI from modules/explorer-api
    - Add explorer property to ElectronAPI interface
    - Add explorer: createExplorerAPI() to createElectronAPI function
    - Export createExplorerAPI function and ExplorerAPI type

--- apps/frontend/src/preload/api/index.ts
+++ apps/frontend/src/preload/api/index.ts
@@ -8,6 +8,7 @@ import { IdeationAPI, createIdeationAPI } from './modules/ideation-api';
 import { InsightsAPI, createInsightsAPI } from './modules/insights-api';
 import { AppUpdateAPI, createAppUpdateAPI } from './app-update-api';
 import { GitHubAPI, createGitHubAPI } from './modules/github-api';
+import { ExplorerAPI, createExplorerAPI } from './modules/explorer-api';
 
 export interface ElectronAPI extends
   ProjectAPI,
@@ -20,6 +21,7 @@ export interface ElectronAPI extends
   InsightsAPI,
   AppUpdateAPI {
   github: GitHubAPI;
+  explorer: ExplorerAPI;
 }
 
 export const createElectronAPI = (): ElectronAPI => ({
@@ -32,7 +34,8 @@ export const createElectronAPI = (): ElectronAPI => ({
   ...createIdeationAPI(),
   ...createInsightsAPI(),
   ...createAppUpdateAPI(),
-  github: createGitHubAPI()
+  github: createGitHubAPI(),
+  explorer: createExplorerAPI()
 });
 
 // Export individual API creators for potential use in tests or specialized contexts
@@ -46,7 +49,8 @@ export {
   createIdeationAPI,
   createInsightsAPI,
   createAppUpdateAPI,
-  createGitHubAPI
+  createGitHubAPI,
+  createExplorerAPI
 };
 
 export type {
@@ -59,5 +63,6 @@ export type {
   IdeationAPI,
   InsightsAPI,
   AppUpdateAPI,
-  GitHubAPI
+  GitHubAPI,
+  ExplorerAPI
 };
